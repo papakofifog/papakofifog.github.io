fb74eb6f88b7244c47ba6729cb31e8cfa2bf42d0
Papa Kofi Akoto Asante <paakofi275@gmail.com>
2022-07-25 19:13:56 +0000

    Increased responsivenes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,83 +1,76 @@
-const header=document.getElementById("header");
+window.onload= function (){
 
-document.addEventListener('scroll',function() {
-    let scrollPos= window.pageYOffset;
+      const header=document.getElementById("header");
 
-    if (scrollPos>=500){
-        header.style.background='rgba(40, 58, 90, 0.9)';
+    document.addEventListener('scroll',function() {
+        let scrollPos= window.pageYOffset;
 
-    }else{
-        header.style.background='transparent';
-    }
-});
+        if (scrollPos>=500){
+            header.style.background='rgba(40, 58, 90, 0.9)';
 
-
-
-var acc = document.getElementsByClassName("spec_day");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-    
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    //console.log(panel);
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-      
-    }
-  });
-}
+        }else{
+            header.style.background='transparent';
+        }
+    });
 
 
-var Lead_section=document.getElementsByClassName("dept");
-var j;
-//Department Profiles
-const pastor_section=document.getElementById("pastors");
-const deacon_section= document.getElementById("deacons");
-const mens_dept_section= document.getElementById("mens-dept");
-const wmen_dept_section= document.getElementById("wmen-dept");
-const youth_dept_section= document.getElementById("y-dept");
-const teen_dept_section= document.getElementById("t-dept");
-const children_dept_section= document.getElementById("c-dept");
 
-// Leadership Variables
-const pastors= "p-lead";
-const deacons= "d-lead";
-console.log(pastors);
-// code to display a particular grid when needed
-for(j=0; j< Lead_section.length; j++){
-  Lead_section[j].addEventListener("click", function(){
-    this.classList.toggle("selected");
-    if (this.getAttribute("id")==pastors){
-        return isShowing(pastor_section);
+
+
+
+    const nav_bar=document.getElementById("nav-button");
+
+    nav_bar.addEventListener("click", function() {
+      if(document.getElementById('nav-container').style.display=="none"){
+        document.getElementById('nav-container').style.display="block"
+        document.getElementById('nav-icon').src="./assets/svg%20icons/cancel-svgrepo-com.svg";
+      }
+      else{
+          document.getElementById('nav-container').style.display="none" ;
+          document.getElementById('nav-icon').src="assets/svg icons/bars-svgrepo-com.svg";
+        }
+    });
+
+
+    var acc = document.getElementsByClassName("spec_day");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
         
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        //console.log(panel);
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+          
+        }
+      });
     }
-    if (this.getAttribute("id")==deacons){
-      return isShowing(deacon_section);
-      
-  }
-  });
+
+    //set and event listener that listens to a click event on either of the navigations that menu container stops displaying.
+    const links_container= document.getElementsByClassName('navigation');
+
+    for (let i=0; i<links_container.length; i++){
+      links_container[i].addEventListener("click", function(){
+        document.getElementById('nav-container').style.display="none" ;
+        document.getElementById('nav-icon').src="./assets/svg icons/bars-svgrepo-com.svg";
+      })
+    }
+
+
+
+
+
+
 }
 
 
 
 
 
-
-
-function isShowing(selected_id){
-  if (selected_id.style.display=="block"){
-    selected_id.style.display=="none";
-    console.log("fail");
-  }else{
-    Lead_section.style.display="none";
-    selected_id.style.display=="block";
-    console.log("succes");
-  }
-}
